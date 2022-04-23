f5335840dee5f9af859cdd1d636b4c7ecd93affc
andreawatkins <awatki22@depaul.edu>
2022-04-23 11:49:56 -0500

    Create Initial Spring/Maven/Java project

---
+++ watchpad-backend/src/main/java/com/watchpad/watchpadbackend/WatchpadBackendApplication.java
@@ -0,0 +1,14 @@
+package com.watchpad.watchpadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchpadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchpadBackendApplication.class, args);
+		System.out.println("This is Watchpad"); 
+	}
+
+}
---
+++ watchpad-backend/src/test/java/com/watchpad/watchpadbackend/WatchpadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchpad.watchpadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchpadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
