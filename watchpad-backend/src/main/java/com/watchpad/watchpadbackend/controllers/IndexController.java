fc7b421e50d025adce8d87aa7ad98f6f016a4bd3
andreawatkins <awatki22@depaul.edu>
2022-05-03 14:45:14 -0500

    implement ci/cd - change maven build action to build and deploy

--- watchpad-backend/src/main/java/com/watchpad/watchpadbackend/controllers/IndexController.java
+++ watchpad-backend/src/main/java/com/watchpad/watchpadbackend/controllers/IndexController.java
@@ -1,4 +1,4 @@
- package com.watchpad.watchpadbackend;
+ package com.watchpad.watchpadbackend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
