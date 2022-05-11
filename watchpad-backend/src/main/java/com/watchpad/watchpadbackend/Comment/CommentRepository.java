0c12a1cdedb554de2e7961e47e1bc762d0697e24
andreawatkins <awatki22@depaul.edu>
2022-05-11 13:44:17 -0500

    create media if doesn't exist when commenting

--- watchpad-backend/src/main/java/com/watchpad/watchpadbackend/Comment/CommentRepository.java
+++ watchpad-backend/src/main/java/com/watchpad/watchpadbackend/Comment/CommentRepository.java
@@ -5,14 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.watchpad.watchpadbackend.User.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-   // @Query("SELECT c FROM Comment c WHERE c.media_id = ?1")
-    //Optional<Comment> findByMediaId(Long id);
+   @Query("SELECT c FROM Comment c WHERE c.media.id = ?1")
+    List<Comment> findByMediaId(Long id);
 
     
 
