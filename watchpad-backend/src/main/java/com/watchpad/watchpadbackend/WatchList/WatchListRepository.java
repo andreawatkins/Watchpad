a353cf91bc5bb6f6860c9f03e11f7299cd1b3b9d
Jonathan Hense <jonathan.hense@gmail.com>
2022-05-11 22:28:27 -0500

    forgot to stage a file. doh

--- watchpad-backend/src/main/java/com/watchpad/watchpadbackend/WatchList/WatchListRepository.java
+++ watchpad-backend/src/main/java/com/watchpad/watchpadbackend/WatchList/WatchListRepository.java
@@ -1,10 +1,14 @@
 package com.watchpad.watchpadbackend.WatchList;
 
+import com.watchpad.watchpadbackend.Media.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WatchListRepository extends JpaRepository<WatchListEntry, Long> {
 
-
+    Optional<List<WatchListEntry>> findByMedia(Media media);
 }
